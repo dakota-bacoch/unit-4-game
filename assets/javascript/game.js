211fc9f3abcaa03ccf0e1f1a2f05ba8cc264ef06
dakota-bacoch <bacoch1998@gmail.com>
2018-06-25 05:19:19 -0700

    changed gem values and score range

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,10 +8,11 @@ $(document).ready(function () {
     var diGem = 0;
     var saGem = 0;
     var totalScore = 0;
-    var possibleNumber = [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
-    var possibleGem = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var possibleNumber = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120];
+    var possibleGem = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
     randomNumber = possibleNumber[Math.floor(Math.random() * possibleNumber.length)];
+    $("#numberInBox").html(randomNumber);
     // Win/Loss Statements
     function winLoss() {
         if (totalScore > 1 && totalScore === randomNumber) {
@@ -21,7 +22,6 @@ $(document).ready(function () {
             $("#numberInBox").html(randomNumber);
             totalScore = 0;
             $("#score").html(totalScore);
-            randomNumber = possibleNumber[Math.floor(Math.random() * possibleNumber.length)];
             while (true) {
                 emGem = possibleGem[Math.floor(Math.random() * possibleGem.length)];
                 ruGem = possibleGem[Math.floor(Math.random() * possibleGem.length)];
@@ -40,7 +40,6 @@ $(document).ready(function () {
             $("#numberInBox").html(randomNumber);
             totalScore = 0;
             $("#score").html(totalScore);
-            randomNumber = possibleNumber[Math.floor(Math.random() * possibleNumber.length)];
             while (true) {
                 emGem = possibleGem[Math.floor(Math.random() * possibleGem.length)];
                 ruGem = possibleGem[Math.floor(Math.random() * possibleGem.length)];
@@ -53,7 +52,7 @@ $(document).ready(function () {
         }
     }
     //Initiate scores
-    
+
     while (true) {
         emGem = possibleGem[Math.floor(Math.random() * possibleGem.length)];
         ruGem = possibleGem[Math.floor(Math.random() * possibleGem.length)];
@@ -69,8 +68,6 @@ $(document).ready(function () {
     $("#win").html(winCount);
     $("#loss").html(lossCount);
     $("#score").html(totalScore);
-
-
 
     //Debug
     console.log(emGem);
@@ -117,11 +114,4 @@ $(document).ready(function () {
         console.log(randomNumber);
         winLoss();
     });
-
-
-
-
-
-
-
 });
